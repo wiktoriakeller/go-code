e81a776a4b18ed9b2b32fc172a95811a9c0ad10e
Wiktoria Keller <65568180+wiktoriakeller@users.noreply.github.com>
2022-08-07 19:06:16 +0200

    Add tests for identity

---
+++ tests/GoCode.UnitTests/Attributes/Customization/ApplicationUserWithoutTokenCustomization.cs
@@ -0,0 +1,14 @@
+﻿using AutoFixture;
+using GoCode.Infrastructure.Identity.Entities;
+
+namespace GoCode.UnitTests.Attributes.Customization
+{
+    public class ApplicationUserWithoutTokenCustomization : ICustomization
+    {
+        public void Customize(IFixture fixture)
+        {
+            fixture.Customize<ApplicationUser>(transform => transform
+                .Without(u => u.RefreshToken));
+        }
+    }
+}
---
+++ tests/GoCode.UnitTests/Infrastructure/IdentityServiceTests.cs
@@ -0,0 +1,171 @@
+﻿using GoCode.Infrastructure.Identity.Entities;
+using Microsoft.AspNetCore.Identity;
+using GoCode.Infrastructure.Interfaces;
+using Microsoft.Extensions.Options;
+using GoCode.Infrastructure.Identity;
+using GoCode.Application.Contracts.DataAccess;
+using GoCode.UnitTests.Attributes;
+using FluentAssertions;
+using GoCode.Application.Identity.Commands;
+using Moq;
+using AutoFixture.Xunit2;
+using GoCode.Infrastructure.Constants;
+using System;
+
+namespace GoCode.UnitTests.Infrastructure
+{
+    public class IdentityServiceTests
+    {
+        [Theory]
+        [ApplicationUserWithoutTokenData]
+        public async Task CreateUserAsync_GivenCreateUserCommand_UsersCountShouldIncrease(CreateUserCommand command,
+            List<ApplicationUser> users)
+        {
+            //Arrange
+            var jwtService = new Mock<IJwtService>();
+            var jwtOptions = Options.Create(new JwtOptions());
+            var refreshTokenRepository = new Mock<IRepository<RefreshToken>>();
+            var userManager = MockUserManager<ApplicationUser>();
+
+            userManager.Setup(x => x.CreateAsync(It.IsAny<ApplicationUser>(), command.Password))
+                .ReturnsAsync(IdentityResult.Success).Callback<ApplicationUser, string>((x, y) => users.Add(x));
+
+            var sut = new IdentityService(userManager.Object,
+                jwtService.Object,
+                refreshTokenRepository.Object,
+                jwtOptions);
+
+            //Act
+            await sut.CreateUserAsync(command);
+
+            //Assert
+            users.Should().HaveCount(4);
+        }
+
+        [Theory]
+        [AutoData]
+        public async Task CreateUserAsync_GivenCreateUserCommand_WhenUserManagerFails_ResponseErrorsShouldNotBeEmpty(CreateUserCommand command)
+        {
+            //Arrange
+            var jwtService = new Mock<IJwtService>();
+            var jwtOptions = Options.Create(new JwtOptions());
+            var refreshTokenRepository = new Mock<IRepository<RefreshToken>>();
+            var userManager = MockUserManager<ApplicationUser>();
+
+            userManager.Setup(x => x.CreateAsync(It.IsAny<ApplicationUser>(), command.Password))
+                .ReturnsAsync(IdentityResult.Failed(new IdentityError { Code = "Identity", Description = "Invalid user data" }));
+
+            var sut = new IdentityService(userManager.Object,
+                jwtService.Object,
+                refreshTokenRepository.Object,
+                jwtOptions);
+
+            //Act
+            var result = await sut.CreateUserAsync(command);
+
+            //Assert
+            result.Errors.Should().NotBeEmpty();
+        }
+
+        [Theory]
+        [AutoData]
+        public async Task AuthenticateUserAync_GivenAuthenticateUserCommand_WhenUserIsNull_ResponseErrorsShouldNotBeEmpty(AuthenticateUserCommand command)
+        {
+            //Arrange
+            var jwtService = new Mock<IJwtService>();
+            var jwtOptions = Options.Create(new JwtOptions());
+            var refreshTokenRepository = new Mock<IRepository<RefreshToken>>();
+            var userManager = MockUserManager<ApplicationUser>();
+
+            userManager.Setup(x => x.FindByEmailAsync(command.Email))
+                .Returns(Task.FromResult<ApplicationUser?>(null));
+
+            var sut = new IdentityService(userManager.Object,
+                jwtService.Object,
+                refreshTokenRepository.Object,
+                jwtOptions);
+
+            //Act
+            var result = await sut.AuthenticateUserAync(command);
+
+            //Assert
+            result.Errors["Identity"].Should().Contain(ErrorMessages.Identity.IncorrectCredentials);
+        }
+
+        [Theory]
+        [ApplicationUserWithoutTokenData]
+        public async Task AuthenticateUserAync_GivenAuthenticateUserCommand_WhenPasswordIsInvalid_ResponseErrorsShouldNotBeEmpty(AuthenticateUserCommand command,
+            ApplicationUser user)
+        {
+            //Arrange
+            var jwtService = new Mock<IJwtService>();
+            var jwtOptions = Options.Create(new JwtOptions());
+            var refreshTokenRepository = new Mock<IRepository<RefreshToken>>();
+            var userManager = MockUserManager<ApplicationUser>();
+
+            userManager.Setup(x => x.FindByEmailAsync(command.Email))
+                .Returns(Task.FromResult(user));
+
+            userManager.Setup(x => x.CheckPasswordAsync(user, command.Password))
+                .ReturnsAsync(false);
+
+            var sut = new IdentityService(userManager.Object,
+                jwtService.Object,
+                refreshTokenRepository.Object,
+                jwtOptions);
+
+            //Act
+            var result = await sut.AuthenticateUserAync(command);
+
+            //Assert
+            result.Errors["Identity"].Should().Contain(ErrorMessages.Identity.IncorrectCredentials);
+        }
+
+        [Theory]
+        [ApplicationUserWithoutTokenData]
+        public async Task AuthenticateUserAync_GivenAuthenticateUserCommand_WhenPasswordIsInvalid_ResponseResultShouldIndicateSuccess(
+            AuthenticateUserCommand command,
+            ApplicationUser user,
+            string token,
+            string jti)
+        {
+            //Arrange
+            var jwtService = new Mock<IJwtService>();
+            var jwtOptions = Options.Create(new JwtOptions());
+            var refreshTokenRepository = new Mock<IRepository<RefreshToken>>();
+            var userManager = MockUserManager<ApplicationUser>();
+
+            userManager.Setup(x => x.FindByEmailAsync(command.Email))
+                .Returns(Task.FromResult(user));
+
+            userManager.Setup(x => x.CheckPasswordAsync(user, command.Password))
+                .ReturnsAsync(true);
+
+            userManager.Setup(x => x.FindByIdAsync(It.IsAny<string>()))
+                .Returns(Task.FromResult<ApplicationUser?>(null));
+
+            jwtService.Setup(x => x.CreateJwtToken(user))
+                .Returns((token, jti));
+
+            var sut = new IdentityService(userManager.Object,
+                jwtService.Object,
+                refreshTokenRepository.Object,
+                jwtOptions);
+
+            //Act
+            var result = await sut.AuthenticateUserAync(command);
+
+            //Assert
+            result.Succeeded.Should().BeTrue();
+        }
+
+        private static Mock<UserManager<TUser>> MockUserManager<TUser>() where TUser : class
+        {
+            var store = new Mock<IUserStore<TUser>>();
+            var userManagerMock = new Mock<UserManager<TUser>>(store.Object, null, null, null, null, null, null, null, null);
+            userManagerMock.Object.UserValidators.Add(new UserValidator<TUser>());
+            userManagerMock.Object.PasswordValidators.Add(new PasswordValidator<TUser>());
+            return userManagerMock;
+        }
+    }
+}
