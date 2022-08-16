2c556d160fd8272396e8c9689ac8dd40cee3e93c
Wiktoria Keller <65568180+wiktoriakeller@users.noreply.github.com>
2022-08-16 12:00:10 +0200

    #9 Move [ApiController] attribute to a common class

--- src/GoCode.WebAPI/Controllers/CoursesController.cs
+++ src/GoCode.WebAPI/Controllers/CoursesController.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Mvc;
 namespace GoCode.WebAPI.Controllers
 {
     [Authorize]
-    [ApiController]
     [Route("api/v1/courses")]
     public class CoursesController : BaseApiController
     {
--- src/GoCode.WebAPI/Controllers/IdentityController.cs
+++ src/GoCode.WebAPI/Controllers/IdentityController.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace GoCode.WebAPI.Controllers
 {
-    [ApiController]
     [Route("api/v1/identity")]
     public class IdentityController : BaseApiController
     {
