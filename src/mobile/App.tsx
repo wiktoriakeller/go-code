039d156549413baaab760ece7e5d2d2046b58cab
Wiktoria Keller <65568180+wiktoriakeller@users.noreply.github.com>
2022-09-07 21:15:11 +0200

    #19 Remove unused page

--- src/mobile/App.tsx
+++ src/mobile/App.tsx
@@ -1,6 +1,5 @@
 import SignInPage from "./pages/SignInPage";
 import RegisterPage from "./pages/RegisterPage";
-import ForgotPasswordPage from "./pages/ForgotPasswordPage";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { RootStackParamList } from "./navigation/navigationTypes";
@@ -18,11 +17,6 @@ export default function App() {
         <Stack.Screen 
           name="Register" 
           component={RegisterPage} 
-        />
-        <Stack.Screen 
-          name="ForgotPassword" 
-          component={ForgotPasswordPage} 
-          options={{ title: "Reset password"}}
         />
       </Stack.Navigator>
     </NavigationContainer>
--- src/mobile/navigation/navigationTypes.ts
+++ src/mobile/navigation/navigationTypes.ts
@@ -3,8 +3,7 @@ import { NativeStackScreenProps } from "@react-navigation/native-stack";
 type RootStackParamList = {
     Login: undefined;
     Register: undefined;
-    ForgotPassword: undefined;
-  };
+};
   
 type LoginNavigation = NativeStackScreenProps<RootStackParamList, "Login">;
 type RegisterNavigation = NativeStackScreenProps<RootStackParamList, "Register">;
